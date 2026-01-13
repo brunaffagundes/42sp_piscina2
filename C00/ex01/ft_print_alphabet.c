bf03a65f9a0bc298c9e3ab77488208c1c6e6ca26
Bruna Fagundes <brunaffagundesqueiroz@gmail.com>
2026-01-13 23:00:46 +0000

    C00

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+    char c;
+
+    c = 'a';
+    while (c <= 'z')
+    {
+        write(1, &c, 1);
+        c++;
+    }
+}
+
+int main(void)
+{
+    ft_print_alphabet();
+    write(1, "\n", 1);
+    return(0);
+}
---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void ft_print_reverse_alphabet(void)
+{
+    char c;
+
+    c = 'z';
+    while(c >= 'a')
+    {
+        write(1, &c, 1);
+        c--;
+    }
+}
+
+int main(void)
+{
+    ft_print_reverse_alphabet();
+    write(1, "\n", 1);
+    return(0);
+}
